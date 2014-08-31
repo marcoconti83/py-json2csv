9a0a471e9178f748c22166c94ec1c78c22608d2f
marcoconti83 <marcoconti83@gmail.com>
2014-08-31 13:40:11 +0200

    Add initial implementation, example data and readme

--- README.md
+++ README.md
@@ -1,4 +1,77 @@
 py-json2csv
 ===========
 
-A JSON to CSV converter in Python
+A JSON to CSV converter made with Python
+
+Copyright (C) Marco Conti 2014 - marcoconti83@gmail.com 
+
+https://www.marco83.com/work
+
+Scope
+------------
+It converts a JSON file with a set of records (array of dictionaries) into a CSV representation of those records.
+
+Details
+-----------
+
+__Flattening vector fields__
+
+Since dictionaries can have some fields that are arrays, the corresponding CSV representation will be flattened. 
+
+E.g.
+the following JSON record
+
+    {
+        "name" : "Marco",
+        "aka" : ["MC", "Marco C"]
+    }
+    
+will be converted to
+
+    # headers
+    name,aka_1,aka2
+    # values
+    Marco,MC,Marco C
+
+__JSON path__
+
+The list of records in the JSON doesn't need to be the top-level object. Nested objects like the one containing the records (`name:A` and `name:B` ) in the following example:
+
+    {
+        "data" : [
+            {
+                "payload" : "foo"
+            },
+            {
+                "records" : [
+                    {
+                        "name" : "A"
+                    },
+                    {  
+                        "name" : "B"
+                    }
+                ]
+            }
+        
+        ]
+    }
+
+can be accessed with the following path syntax:
+
+    data,1,records
+    
+which means: `data` field, second element (0-based indexing), `records` field
+
+Sample usage
+------------
+Type
+
+    ./json2csv.py --help
+	
+for a list of options
+
+Try:
+
+    ./json2csv.py -o o.txt -p queries,1,results -n "{fieldName}_n{index}" sample.json
+
+It will convert the `sample.json` into `o.txt`
---
+++ json2csv.py
@@ -0,0 +1,189 @@
+#!/usr/bin/python
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# Copyright Marco Conti 2014 - marcoconti83@gmail.com
+
+# Converts JSON to CSV flattening any array into multiple CVS fields
+# See readme.md for documentation / usage example
+
+import json
+import sys
+import csv
+import argparse
+
+SUPPORTED_SCALAR_TYPES = [basestring, long, int, bool, float]
+SUPPORTED_VECTOR_TYPES = [list, tuple]
+
+
+##########################################################################
+def die(*args):
+    '''Prints an error message and exits'''
+    print >> sys.stderr, "ERROR:", " ".join(map(str, args))
+    exit(1)
+
+
+###########################################################################
+def isSupportedScalarType(value):
+    '''Checks whether the variable is a supported scalar type'''
+    for t in SUPPORTED_SCALAR_TYPES:
+        if isinstance(value, t):
+            return True
+    return False
+
+def isSupportedVectorType(value):
+    '''Checks whether the variable is a supported scalar value'''
+    for t in SUPPORTED_VECTOR_TYPES:
+        if isinstance(value, t):
+            # now check that internal values are scalar
+            for i in value:
+                if not isSupportedScalarType(i):
+                    return False
+            return True
+            
+    return False
+
+def getValueAtPath(dictionary, path):
+    '''
+    Returns the value at the given path. Path is a list. 
+    e.g. [a, b, c] will get the path at dictionary[a][b][c]
+    '''
+    val = dictionary
+    for i in path:
+        if isinstance(val, (list, tuple)):
+            if i.isdigit():
+                val = val[int(i)]
+            else:
+                die("Path",path,"at position",i,"expects a number as the JSON contains an array")
+        else:
+            val = val[i]
+    return val
+
+##########################################################################
+class Json2CSVConverter(object):
+    
+    '''Converts a JSON into a CVS'''
+    def __init__(self, file):
+        self.__json = self.__loadFromFile(file)
+        self.__fields = {} # field name (str) to field cardinality (int)
+        self.arrayFieldNamingFormat = "{fieldName}_{index}"
+        
+    def __vectorialFieldName(self, field, index):
+        '''Returns the name of the field for a vector field value in position index'''
+        return self.arrayFieldNamingFormat.format(fieldName=field,index=index)
+        
+    def __loadFromFile(self, file):
+        with open(file, "r") as FILE:
+            return json.load(FILE)
+    
+    def __extractHeaders(self, dictionaries):
+        '''Extract the list of headers for the CVS'''
+        for d in dictionaries:
+            for key in d.iterkeys():
+                value = d[key]
+
+                if isSupportedVectorType(value):
+                    self.__addFieldHeader(key, max(len(value),1))
+                    continue
+                    
+                if isSupportedScalarType(value):
+                    self.__addFieldHeader(key, 1)
+                    continue
+                    
+                die("Unknown type for value {}: {} ({})".format(key, value, type(value)))
+    
+    def __createFieldsDictionary(self):
+        '''Returns a dictionary with keys from the headers list'''
+        dictionary = {}
+        for (field, cardinality) in self.__fields.items():
+            if(cardinality == 1):
+                dictionary[field] = None
+            else:
+                for i in xrange(cardinality):
+                    name = self.__vectorialFieldName(field, i)
+                    dictionary[name] = None
+        return dictionary
+    
+    def __recordsList(self, dictionaries):
+        '''Return a list of records'''
+        
+        records = []
+        for values in dictionaries:
+            record = self.__createFieldsDictionary()
+            for (field, value) in values.items():
+                if self.__fields[field] > 1:
+                    # vector type
+                    for i in xrange(len(value)):
+                        name = self.__vectorialFieldName(field, i)
+                        record[name] = value[i]
+                # scalar type or size-1 array
+                else:
+                    if isSupportedVectorType(value):
+                        if len(value) > 0:
+                            value = value[0]
+                        else:
+                            value = None
+                    if isinstance(value, unicode):
+                        value = value.encode('ascii', errors='backslashreplace')
+                    record[field] = value
+                    
+            # now sort record by keys
+            sortedRecord = []
+            for k in sorted(record.keys()):
+                sortedRecord.append(record[k])
+                
+            records.append(sortedRecord)
+        return records
+        
+    
+    def __flatten(self, dictionaries, file):
+        '''Extracts a list of headers from an array of dictionaries'''
+        self.__extractHeaders(dictionaries)
+        records = self.__recordsList(dictionaries)
+
+        sortedHeaders = sorted(self.__createFieldsDictionary().keys())
+        
+        with open(file, 'wb') as FILE:
+            writer = csv.writer(FILE)
+            writer.writerow(sortedHeaders)
+            writer.writerows(records)
+            
+        print len(records), "records written to", file
+            
+    def __addFieldHeader(self, fieldName, cardinality):
+        '''Adds a field to the list, with the given cardinality. If the field exists already, uses the max cardinality between the two'''
+        previous = self.__fields.get(fieldName, 1)
+        self.__fields[fieldName] = max(cardinality, previous)
+        
+    def cvs(self, JSONpath, outputFile):
+        '''Saves a CVS representation of an array in the JSON. JSON is a path (see 'getValueAtPath')'''
+        self.__fields = {}
+        array = getValueAtPath(self.__json, JSONpath)
+        if not isinstance(array, (list,tuple)):
+            die("Requested path", "->".join(map(str,JSONpath)), "is of unsupported type", type(array))
+        for d in array:
+            if not isinstance(d, dict):
+                die("Requested path", "->".join(map(str,JSONpath)), "contains a non-dictionary element", d)
+        
+        self.__flatten(array, outputFile)
+    
+##############################################################################
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser(description="Converts a JSON file with an array of records to a CSV (comma separated values) file")
+    parser.add_argument("json_file", help="The json input file")
+    parser.add_argument("-o", "--output-file", help="The csv output file", default="out.csv")
+    parser.add_argument("-p", "--json-path", help="Path (comma-separated) inside the JSON to the array of records to converts, e.g. 'results,array' will point to the first-level field 'results' and then to its subfield 'array'. Leave empty only if the entire JSON is already an array of records", default=None)
+    parser.add_argument("-n", "--array-field-naming", help="Format for naming of sub-fields for array-like fields. Use '{fieldName}' as a placeholder of the field name, and {index} as a placeholder for the sub-field index. This string is used in Python's str.format() function, please refer to the Python documentation for more advanced usages", default="{fieldName}_{index}")
+    
+    args = parser.parse_args()
+    
+    converter = Json2CSVConverter(args.json_file)
+    converter.arrayFieldNamingFormat = args.array_field_naming
+    path = []
+    if args.json_path:
+        path = args.json_path.split(',')
+    
+    converter.cvs(path, args.output_file)
